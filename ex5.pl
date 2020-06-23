bb572bd9c11688392aebae4b64311da6a0857e23
TomerAmran <50573231+TomerAmran@users.noreply.github.com>
2020-06-23 12:46:10 +0300

    pickmeup done

--- ex5.pl
+++ ex5.pl
@@ -69,8 +69,9 @@ not_member(X, [Y|Ys]) :- X \= Y,
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% To Do
 
 % Signature: pick_me_up(Child_name,Phone)/2
-% Purpose:
-
+% Purpose:  defines the relation between a child name and its parent phone number, when the parent has a car
+pick_me_up(Child_name,Phone) :- parents(Child_name,X,Y), parent_details(X, Phone, true).
+pick_me_up(Child_name,Phone) :- parents(Child_name,X,Y), parent_details(Y, Phone, true).
 
 % Signature: active_child(Name)/1
 % Purpose:
